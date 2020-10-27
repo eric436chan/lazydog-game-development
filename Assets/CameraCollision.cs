430a19486f8ed6ff690293ead7f88b3596deebb2
eric436chan <eric436.chan@gmail.com>
2020-10-27 01:34:13 -0700

    fixed clipping camera

---
+++ Assets/CameraCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    Vector3 originalPosition;
+    Vector3 finalPosition = new Vector3(-0.367f, 1.1f, 0.25f);
+
+
+    private void Start()
+    {
+        originalPosition = transform.localPosition;
+        Debug.Log(originalPosition);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.SphereCast(transform.position, 0.5f, transform.forward, out hit, 2f))
+        {
+            
+            transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition, 0.1f);
+        }
+        else if(transform.localPosition != originalPosition)
+        {
+           
+            transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, 0.1f);
+        }
+        
+       
+    }
+}
