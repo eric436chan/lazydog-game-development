74055b60c5edadc34bbc4885ff704eef30ea1ef4
eric436chan <eric436.chan@gmail.com>
2020-10-28 16:49:37 -0700

    added menus

---
+++ Assets/Scripts/Menu/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loader;
+    public Slider slider;
+    public TextMeshProUGUI percentageText;
+   
+   public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+        
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loader.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            percentageText.text = (slider.value*100).ToString() + "%";
+          
+            yield return null;
+        }
+    }
+  
+}
---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPaused)
+        {
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenu.SetActive(false);
+        isPaused = false;
+       
+    }
+
+    public void Pause()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+       
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        
+    }
+}
