de5c751d67e932b11f868363aa6c39cfd57e59af
Ali Sherman <aliannea.sherman@gmail.com>
2020-10-31 23:26:41 -0700

    Fixed Inventory UI
    Fixed Inventory UI, now adds Saw and ScrewDriver

--- Assets/Scripts/Game/Inventory.cs
+++ Assets/Scripts/Game/Inventory.cs
@@ -6,19 +6,15 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour
 {
     //GameObject selected = null;
-    private int current_num_of_items;
-
-    private Item item;
-
     public Sprite[] sprites;
+    private GameObject inventory_slot0;
     private GameObject inventory_slot1;
     private GameObject inventory_slot2;
     private GameObject inventory_slot3;
     private GameObject inventory_slot4;
-    private GameObject inventory_slot0;
+    
     private Image child = null;
 
-    private int inventory_slot;
 
     #region Singleton
 
@@ -42,9 +38,8 @@ public class Inventory : MonoBehaviour
     private void Start()
     {
         LoadIcons();
-        current_num_of_items = 0;
-        inventory_slot = 0;
-        inventory_slot0 = GameObject.Find("inventory_slot1");
+
+        inventory_slot0 = GameObject.Find("inventory_slot0");
         inventory_slot1 = GameObject.Find("inventory_slot1");
         inventory_slot2 = GameObject.Find("inventory_slot2");
         inventory_slot3 = GameObject.Find("inventory_slot3");
@@ -66,7 +61,6 @@ public class Inventory : MonoBehaviour
 
     public void addItem(Item item)
     {
-        this.item = item;
         if (inventory.Count < inventorySpace)
         {
             inventory.Add(item);
@@ -76,49 +70,37 @@ public class Inventory : MonoBehaviour
 
     private void updateInventoryItem(string name)
     {
-        switch (current_num_of_items)
+        switch (inventory.Count-1)
         {
             case 0:
                 Debug.Log("Case 0");
-
                 //gets the 0th child of the inventory slot, the icon
                 child = inventory_slot0.transform.GetChild(0).GetComponent<Image>();
-                inventory_slot = 0;
-
                 break;
 
             case 1:
                 Debug.Log("Case 1");
                 child = inventory_slot1.transform.GetChild(0).GetComponent<Image>();
-                inventory_slot = 1;
-                //current_num_of_items += 1;
                 break;
 
             case 2:
                 Debug.Log("Case 2");
-                inventory_slot = 2;
                 child = inventory_slot2.transform.GetChild(0).GetComponent<Image>();
-                //current_num_of_items += 1;
                 break;
 
             case 3:
                 Debug.Log("Case 3");
-                inventory_slot = 3;
                 child = inventory_slot3.transform.GetChild(0).GetComponent<Image>();
-                //current_num_of_items += 1;
                 break;
 
             case 4:
                 Debug.Log("Case 4");
-                inventory_slot = 4;
                 child = inventory_slot4.transform.GetChild(0).GetComponent<Image>();
-                //current_num_of_items += 1;
                 break;
 
             default:
                 Debug.Log("Empty");
                 break;
-                current_num_of_items += 1;
         }
 
         //Debug.Log("hi");
@@ -126,6 +108,24 @@ public class Inventory : MonoBehaviour
         {
             Debug.Log(sprites[8].ToString());
             child.sprite = sprites[8];
+
+            Color temp = child.color;
+            temp.a = 1f;
+            child.color = temp;
+        }
+        if (name.Contains("Saw"))
+        {
+            Debug.Log(sprites[1].ToString());
+            child.sprite = sprites[1];
+
+            Color temp = child.color;
+            temp.a = 1f;
+            child.color = temp;
+        }
+        if (name.Contains("Screwdriver"))
+        {
+            Debug.Log(sprites[5].ToString());
+            child.sprite = sprites[5];
 
             Color temp = child.color;
             temp.a = 1f;
