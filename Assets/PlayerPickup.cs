f799d4e81b44e3c4bc25f22bd1c46e876db18942
eric436chan <eric436.chan@gmail.com>
2020-12-03 16:05:53 -0800

    fixed bugs

--- Assets/PlayerPickup.cs
+++ Assets/PlayerPickup.cs
@@ -7,28 +7,36 @@ public class PlayerPickup : MonoBehaviour
     public CharacterController controller;
     private float dist;
     public Transform destination;
+    private bool selected = false;
 
     // Update is called once per frame
     private void Update()
     {
         dist = Vector3.Distance(controller.transform.position, gameObject.transform.position);
-    }
 
-    private void OnMouseOver()
-    {
-        if (dist <= 7f)
+        if (dist <= 5f && selected)
         {
             if (Input.GetMouseButton(0))
             {
                 gameObject.GetComponent<Rigidbody>().useGravity = false;
-                gameObject.transform.position = destination.position;
-                transform.parent = GameObject.Find("Destination").transform;
+                gameObject.transform.position = controller.transform.position + controller.transform.forward * 3f;
+                gameObject.transform.rotation = new Quaternion(0f, controller.transform.rotation.y, 0f, controller.transform.rotation.w);
             }
             else
             {
-                transform.parent = null;
+                selected = false;
                 gameObject.GetComponent<Rigidbody>().useGravity = true;
             }
         }
     }
+
+    private void OnMouseOver()
+    {
+        selected = true;
+    }
+
+    //private void OnMouseExit()
+    //{
+    //    selected = false;
+    //}
 }
